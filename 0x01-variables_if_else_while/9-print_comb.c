cce0bc7ba688dafa9a2b61bd30e5d3bd671c096e
adel484 <tomasander12@gmail.com>
2023-02-17 11:38:43 -0800

    sec day

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,18 @@
 int main(void)
 {
 	int i;
-	
 	i = 48;
+
 	while (i <= 57)
 	{
 		putchar(i);
-		putchar(' ');
-		putchar(',');
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		i++;
 	}
 	putchar('\n');
-	return (0);
-		
+	return (0);	
 }
