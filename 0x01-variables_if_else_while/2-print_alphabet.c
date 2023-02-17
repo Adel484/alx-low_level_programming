d4571b9a99ac7ea6cb51c722d0eec039475fe7cc
adel484 <tomasander12@gmail.com>
2023-02-17 10:14:32 -0800

    sec day

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alpha
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str[25];
+	int i;
+
+	for (i=0; i < 26; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar("\n");
+	return (0);
+}
