608bc2ea3d42639ec39ae16122842bae369e5d80
adel484 <tomasander12@gmail.com>
2023-02-17 11:31:15 -0800

    sec day

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - printing combination
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		putchar(' ');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+		
+}
