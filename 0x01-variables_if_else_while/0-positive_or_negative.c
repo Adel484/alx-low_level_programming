0c6deb4885ac12da5f3e07e9b7c0ee7b0a5455bb
adel484 <tomasander12@gmail.com>
2023-02-17 08:44:52 -0800

    check sign

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0){
+	if (n > 0)
+	{
 		puts("%d is positve", n);
 	}
-	else if (n==0){
+	else if (n == 0)
+	{
 		puts("%d is zero", n);
 	}
-	else {
+	else
+        {
 		puts("%d is negative", n);
 	}
 	
