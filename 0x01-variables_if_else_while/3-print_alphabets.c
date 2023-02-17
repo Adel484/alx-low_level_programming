07bdf927faff846ac8ccb21440b820ca446edc1c
adel484 <tomasander12@gmail.com>
2023-02-17 10:33:16 -0800

    sec day

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,17 @@
 
 int main(void)
 {
-        char i;
+	char i;
 	char b;
 
-        for (i = 'a'; i <= 'z'; i++)
-        {
-                putchar(i);
-        }
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
 	for (b = 'A'; b <= 'Z'; b++)
 	{
 		putchar(b);
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
