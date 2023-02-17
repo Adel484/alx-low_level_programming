bac955cb6cc7679a99b075d6b6097aaa676a22f3
adel484 <tomasander12@gmail.com>
2023-02-17 10:56:24 -0800

    sec day

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int _;
+
+	for (_ == 0; _ < 10; _++)
+		putchar(_);
+	putchar('\n');
+	return (0);
+}
