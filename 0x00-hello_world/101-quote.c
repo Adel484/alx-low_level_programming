b743dd25d50f3db925bea0123ee02851fca49ac5
adel484 <tomasander12@gmail.com>
2023-02-18 10:57:50 -0800

    nice

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - printing without printf or puts
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char s;
+
+	s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	for (i = 0; i <= 58; i++)
+	{
+		putchar(s[i]);
+	}
+	putchar("\n");
+	return (1);
+}
