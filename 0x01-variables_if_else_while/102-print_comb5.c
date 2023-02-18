555b43ec96ae35c8d4e6d008bb0d132da757967e
adel484 <tomasander12@gmail.com>
2023-02-18 14:19:06 -0800

    fjasd

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - printing nums
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i, j, k, m;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = i; k <= 57; k++)
+			{
+				for (m = 48; m <= 57; m++)
+				{
+					if (i == j && j == k && k == m)
+					{
+						continue;
+					}
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(m);
+				/*	if (i != 56) */
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
