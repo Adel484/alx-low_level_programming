1a4da7ccb244e7a942d10ec1fff6316be43c78c6
adel484 <tomasander12@gmail.com>
2023-02-21 13:48:50 -0800

    as

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <main.h>
+#include <ctype.h>
+
+/**
+ * _isalpha - check alpha
+ *
+ * @c: parameter
+ *
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+	_putchar('\n')
+}
