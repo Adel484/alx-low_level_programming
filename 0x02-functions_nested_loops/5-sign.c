3d6c9e527f5c640d8a565fa4e7de11d1a654a055
adel484 <tomasander12@gmail.com>
2023-02-21 14:04:13 -0800

    asd

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - check
+ * @n: h
+ * Return: 0 or 1 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else if (n > 1)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n < 1)
+	{
+		return (-1);
+		_putchar('-');
+	}
+	_putchar('\n')
+}
