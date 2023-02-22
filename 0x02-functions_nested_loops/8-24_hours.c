6acb0684e6e12f65d54df273c1292f3e6a084033
adel484 <tomasander12@gmail.com>
2023-02-21 16:08:29 -0800

    hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - my day
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j < 52; j++)
+		{
+			for (k = 48; k < 56; k++)
+			{
+				for (l = 0; l < 58; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+				}
+			}
+		}
+	}
+}
