0fd0d93055b791cae8c477628efcf78fd3c41e19
adel484 <tomasander12@gmail.com>
2023-02-21 17:10:42 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,22 @@
 
 void print_to_98(int n)
 {
-	for (n = 0; n < 99; n++)
+	while (n < 98)
 	{
 		printf("%d", n);
 		if (n == 98)
 			continue;
 		putchar(',');
 		putchar(' ');
+		n++
+	}
+	while (n > 98)
+	{
+		if (n == 98)
+			continue;
+		putchar(',');
+		putchar(' ');
+		n--
 	}
 	printf("\n");
 }
