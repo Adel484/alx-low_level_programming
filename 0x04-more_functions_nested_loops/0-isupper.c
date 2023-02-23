4083bcbbbc1124f9c27e682f54623b055a52a841
adel484 <tomasander12@gmail.com>
2023-02-23 11:55:05 -0800

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper - check whether the c is upper or not 
+ * _isupper - check whether the c is upper or not
  * @c: parameter
  * Return: 0 or 1
  */
@@ -8,7 +8,7 @@
 int _isupper(int c)
 {
 	if (c >= 97 && c <= 122)
-		retrurn (1);
+		return (1);
 	else
 		return (0);
 }
