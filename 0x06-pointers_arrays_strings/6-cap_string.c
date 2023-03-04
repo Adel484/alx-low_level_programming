acd5c8baa38da9598b3f2c9f9c724c4048e89255
adel484 <tomasander12@gmail.com>
2023-03-04 03:21:42 -0800

    captialize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,14 +11,14 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		if (str[i] >= 91 && str[i] <= 122)
-		{
-			if (str[i - 1] >= 32 && str[i - 1] <= 63)
-				str[i] -= 32;
-			else if (str[i - 2] == '\\')
-				str[i] -= 32;
-			else if (str[i - 1] == '{' || str[i - 1] == '}')
-				str[i] -= 32;
+		while (!(str[i] >= 91 && str[i] <= 122))
+			i++;
+		if (str[i - 1] >= 32 && str[i - 1] <= 63)
+			str[i] -= 32;
+		else if (str[i - 2] == '\\')
+			str[i] -= 32;
+		else if (str[i - 1] == '{' || str[i - 1] == '}')
+			str[i] -= 32;
 		}
 		i++;
 	}
