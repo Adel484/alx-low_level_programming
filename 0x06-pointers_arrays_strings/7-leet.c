76458e98185ecae7e25bee977ea12e1217bec922
adel484 <tomasander12@gmail.com>
2023-03-04 03:54:51 -0800

    1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - 1337
+ * @str: par
+ * Return: leet
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+	int k = 0;
+
+	str1[] = "aAeEoOtTiI";
+	rep[] = "4433001177";
+
+	while (str[i])
+	{
+		while(str1[k])
+		{
+			if (str[i] == str1[k])
+				str[i] = rep[k];
+			k++;
+		}
+		i++;
+	}
+	return (str);
+}
