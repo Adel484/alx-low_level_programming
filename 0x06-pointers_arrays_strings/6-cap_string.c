d525bce8ebe1c3510f1a16df1bf4e8101c4adba6
adel484 <tomasander12@gmail.com>
2023-03-04 02:12:32 -0800

    captialize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * cap_string - capitalize strings
+ * @str: par
+ * Return: capitalize string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 91 && str[i] <= 122)
+		{
+			if (str[i - 1] >= 32 && str[i - 1] <= 90)
+				str[i] -= 32;
+			else if (str[i - 1] == '{' || '}')
+				str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
