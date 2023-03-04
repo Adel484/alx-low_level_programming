911ea6861dfa2e1350e774cbd6e125691197e45b
adel484 <tomasander12@gmail.com>
2023-03-04 04:32:08 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rot13 - encode str to rot13
+ * @str: string to be encoded
+ * Return: encoded str
+ */
+
+char *rot13(char *str)
+{
+	int i = 0, k;
+
+	while (str[i])
+	{
+		for (k >= 'a'; k <= 'z'; k++)
+		{
+			if (str[i] == alpha[k] && k < 'm')
+				str[i] = alpha[k + 13];
+			else
+				str[i] = alpha[k - 13];
+		}
+		i++;
+	}
+}
